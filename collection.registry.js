1a9e6eaf565deed2b0fb1d80eda76e1da2aef53d
Kobita <161524322+kobita1122@users.noreply.github.com>
2026-01-18 14:54:38 +0600

    Create collection.registry.js

---
+++ collection.registry.js
@@ -0,0 +1,11 @@
+export function loadCollections() {
+  console.log("Loading NFT collections...");
+
+  return [
+    {
+      name: "Genesis Apes",
+      floorPrice: 1.5,
+      volume24h: 120
+    }
+  ];
+}
